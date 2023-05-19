ae992c629543e7b90e3f6fee9b2b0c43ddaac13c
Gabriela Yasmin <84574158+gabyyasmindesouza@users.noreply.github.com>
2023-05-19 09:31:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//Atividade 1--------------------------------------------------------------------
+// let nums = prompt()
+// function n(nums){
+//     let lista = nums.split("")
+//     console.log(lista.reverse())
+// }
+// n(nums)
+
+//Atividade 2--------------------------------------------------------------------
+// function ola() {
+//     console.log("Bom dia, Mundo!!!")
+// }
+// ola()
+
+//Atividade 3------------------------------------------------------------------
+// const palavra = prompt("Digite uma palavra")
+// function vogais(palavra){
+//     const lista = []
+//     lista.push(palavra)
+//     console.log(lista.length())
+// }
+// vogais(palavra)
+
+//Atividade 4--------------------------------------------------------------------
+// let num = Number(prompt())
+// function a(num){
+//    for(let i = 2; i < num; i++){
+//     if (num % i === 0){
+//         return ("Este número não é primo")
+//     }
+//    }
+//    return ("Este número é primo")
+// }
+// console.log(a(num))
